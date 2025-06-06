525efb49fe6f27a3dc18948fe3e3eabf07eb8c96
Lawrence Gray <ljg2305@gmail.com>
2025-06-06 19:23:00 +0100

    smoke test hamming encode

--- testbench/hamming_encode_testbench.py
+++ testbench/hamming_encode_testbench.py
@@ -2,8 +2,45 @@ import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge
 
+from functools import reduce 
+import operator as op 
+import numpy as np
+
 @cocotb.test
 async def hamming_test(dut):
+
+
+    #set bits constrain to data width 
+    #insert blank space at 2^^i
+    #calcualte xor reduction 
+    #insert bits 
+    #calculate extended parity 
+    #np.random.seed(1023930)
+
+    data_width = 32
+    bits = np.random.randint(0,2,data_width)
+
+    input = int(bits.dot(1 << np.arange(bits.shape[-1])))
+
+
+    k = data_width
+    r=0
+    while (2 ** r) < (k + r + 1):
+        r += 1
+
+    bits = np.insert(bits,0,0)
+    for i in range(r):
+        bits = np.insert(bits,2**i,0)
+
+
+    parity_bits = bin(reduce(op.xor,[i for i, bit in enumerate(bits) if bit]))
+
+    for i, parity_bit in enumerate(parity_bits[:1:-1]):
+        bits[2**i] = parity_bit
+
+    bits[0] = reduce(op.xor,[bit for bit in bits])
+    result = bits.dot(1 << np.arange(bits.shape[-1]))
+
     #generate clock 
     cocotb.start_soon(Clock(dut.clk,1,units="ns").start()) 
 
@@ -17,10 +54,22 @@ async def hamming_test(dut):
         await RisingEdge(dut.clk)
     
     dut.rst_n.value = 1
-    dut.data_in.value = 22927655 
+    dut.data_in.value = input
     
     for count in range(10):
         await RisingEdge(dut.clk)
     
+    
     print(dut.data_out.value.binstr)
+    print("".join([str(i) for i in np.flip(bits)]))
+    print(dut.data_out.value.integer)
+    print(result)
+
+    assert dut.data_out.value.integer == result, \
+    "Error, expected %s, got %s" % (dut.data_out.value.integer,result)
   
+    parity_bits = bin(reduce(op.xor,[i for i, bit in enumerate(bits) if bit]))
+    print(parity_bits)
+    bits[9] = 0 if bits[9] else 1 
+    parity_bits = bin(reduce(op.xor,[i for i, bit in enumerate(bits) if bit]))
+    print(parity_bits)
