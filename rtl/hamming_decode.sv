ecfad2fcfe19e13902c67d8e394ee1311943a5c6
Lawrence Gray <ljg2305@gmail.com>
2025-06-10 08:14:28 +0100

    initial smoke test for hamming_decode

--- rtl/hamming_decode.sv
+++ rtl/hamming_decode.sv
@@ -19,9 +19,12 @@ parameter PADDED_CODE_WIDTH = 2 ** (ADDR_WIDTH);
 
 logic [CODED_WIDTH-1:0][ADDR_WIDTH-1:0] parity_input; 
 logic [CODED_WIDTH-1:0][ADDR_WIDTH-1:0] parity_index; 
+logic [CODED_WIDTH-1:0] fixed_data_in; 
 logic [ADDR_WIDTH-1:0] parity_bits;
 
 logic [DATA_WIDTH-1:0] unpacked_input;
+
+logic exteded_parity;
 
 // TODO put in to module to be reused by decoder
 // hamming_parity
@@ -38,28 +41,38 @@ always_comb begin
   end
 end
 
+assign exteded_parity = ^data_in[CODED_WIDTH-1:1];
+always_comb begin
+    case ({|parity_bits, exteded_parity})
+        2'b00: num_errors = 2'b00; // No errors
+        2'b01: num_errors = 2'b10; // 2 errors
+        2'b10: num_errors = 2'b01; // 1 error
+        2'b11: num_errors = 2'b10; // 2 errors
+    endcase
+end
+
+assign location   = parity_bits;
+always_comb begin
+    fixed_data_in = data_in;
+    if (num_errors == 1) begin 
+        fixed_data_in[location] = !data_in[location];
+    end 
+end
+
 genvar i; 
 generate
 for (i = 0; i < ADDR_WIDTH; i++) begin
+    // TODO: Fix for non power of 2 coded data width 
     localparam int current_pow = 2 ** i;
     localparam int next_pow = 2 ** (i + 1);
     localparam int width = next_pow - current_pow - 1;
     localparam int data_offset   = current_pow - (i+1);
     localparam int packed_offset = current_pow + 1;
 
-
     if (width  > 0)
-        assign unpacked_input[data_offset +: width] = data_in[packed_offset+: width];
+        assign unpacked_input[data_offset +: width] = fixed_data_in[packed_offset+: width];
 end
 endgenerate
-
-assign exteded_parity = ^data_in[CODED_WIDTH-1:1];
-always_comb begin 
-    num_errors = 2'b00; // can be 0, 1 or 2 
-    // TODO add logic for extended parity
-    
-end
-assign location   = partiy_bits;
 
 assign data_out = unpacked_input;
 
---
+++ testbench/hamming_decode_testbench.py
@@ -0,0 +1,70 @@
+import cocotb 
+from cocotb.clock import Clock
+from cocotb.triggers import RisingEdge
+
+from functools import reduce 
+import operator as op 
+import numpy as np
+
+@cocotb.test
+async def hamming_test(dut):
+
+    data_width = 25
+    bits = np.random.randint(0,2,data_width)
+
+    result_bits = bits.copy()
+    result = int(bits.dot(1 << np.arange(bits.shape[-1])))
+
+
+    k = data_width
+    r=0
+    while (2 ** r) < (k + r + 1):
+        r += 1
+
+    bits = np.insert(bits,0,0)
+    for i in range(r):
+        bits = np.insert(bits,2**i,0)
+
+
+    parity_bits = bin(reduce(op.xor,[i for i, bit in enumerate(bits) if bit]))
+
+    for i, parity_bit in enumerate(parity_bits[:1:-1]):
+        bits[2**i] = parity_bit
+
+    bits[0] = reduce(op.xor,[bit for bit in bits])
+
+    bits[9] = 0 if bits[9] else 1 
+    input = bits.dot(1 << np.arange(bits.shape[-1]))
+
+    #generate clock 
+    cocotb.start_soon(Clock(dut.clk,1,units="ns").start()) 
+
+    dut.rst_n.value = 1
+    dut.data_in.value = 0
+
+    # reset dut
+    dut.rst_n.value = 0
+
+    for _ in range(2):
+        await RisingEdge(dut.clk)
+    
+    dut.rst_n.value = 1
+    dut.data_in.value = int(input)
+    
+    for count in range(10):
+        await RisingEdge(dut.clk)
+    
+    
+    print(dut.data_out.value.binstr)
+    print("".join([str(i) for i in np.flip(result_bits)]))
+    print(dut.data_out.value.integer)
+    print(result)
+
+    assert dut.data_out.value.integer == result, \
+    "Error, expected %s, got %s" % (dut.data_out.value.integer,result)
+  
+    parity_bits = bin(reduce(op.xor,[i for i, bit in enumerate(bits) if bit]))
+    print(parity_bits)
+    bits[9] = 0 if bits[9] else 1 
+    parity_bits = bin(reduce(op.xor,[i for i, bit in enumerate(bits) if bit]))
+    print(parity_bits)
